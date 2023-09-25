bd18e66f616ea5a2d611ec7e702d1836d116de98
TomyMarengo <marengotomas@gmail.com>
2023-09-25 00:10:22 -0300

    Animation

--- animation.py
+++ animation.py
@@ -1,117 +1,70 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 from matplotlib.animation import FuncAnimation
-from constants import constants
 
-##### Graphics #####
-img_plot = None
-ax2d = None
-minor_height = 0
+num_particles = 15
+delta_t = 0.01
+tf = 5
+particle_radius = 0.0225
+line_length = 1.35
+
+fig, ax = plt.subplots()
+ax.set_xlim(0, line_length)
+ax.set_ylim(-particle_radius, particle_radius)
+
+# Circles:
+ax.set_aspect('equal')
+# Line
+ground_line, = ax.plot([0, line_length], [0, 0], 'k-', lw=2)
+
+particle_positions = np.array([0.0] * (num_particles * 2))
+particles = []
 lines = []
-n = constants["n"]
-radius = constants["radius"]
-mass = constants["mass"]
-init_velocity = constants["init_velocity"]
-main_width = constants["main_width"]
-main_height = constants["main_height"]
-minor_width = constants["minor_width"]
-
-####################
-
-def update(frame, skip):
-    global img_plot, ax2d, minor_height, lines
-
-    ax2d.clear()
-
-    first_line = skip * frame * (n + 4)  # 4 -> step + time + firstObjectColliding + secondObjectColliding
-    step = int(lines[first_line].split()[1])
-    time = float(lines[first_line + 1].split()[1])
-    firstObjectColliding = int(lines[first_line + 2].split()[1])
-    secondObjectColliding = str(lines[first_line + 2].split()[2])
-
-    # Initialize array particles, each line of the file contain posX posY velX velY of each particle
-    particles = np.zeros((n, 4))
-    for i in range(n):
-        particles[i] = lines[first_line + 3 + i].split()
-
-    img_plot = ax2d.scatter(particles[:, 0], particles[:, 1], s=50, c='b')
-
-    ax2d.plot([0, 0], [-main_height, 0], color='b')  # 0
-    # line from (0, 0) to (main_width, 0)
-    ax2d.plot([0, main_width], [0, 0], color='b')  # 1
-    # line from (main_width, 0) to (main_width, -(main_height - minor_height) / 2
-    ax2d.plot([main_width, main_width], [0, -(main_height - minor_height) / 2], color='b')  # 2
-    # line from (main_width, -(main_height - minor_height) / 2) to (main_width + minor_width, -(main_height - minor_height) / 2)
-    ax2d.plot([main_width, main_width + minor_width],
-              [-(main_height - minor_height) / 2, -(main_height - minor_height) / 2], color='b')  # 3
-    # line from (main_width + minor_width, -(main_height - minor_height) / 2) to (main_width + minor_width, -(main_height - minor_height) / 2 - minor_height)
-    ax2d.plot([main_width + minor_width, main_width + minor_width],
-              [-(main_height - minor_height) / 2, -(main_height - minor_height) / 2 - minor_height], color='b')  # 4
-    # line from (main_width + minor_width, -(main_height - minor_height) / 2 - minor_height) to (main_width, -(main_height - minor_height) / 2 - minor_height)
-    ax2d.plot([main_width + minor_width, main_width],
-              [-(main_height - minor_height) / 2 - minor_height, -(main_height - minor_height) / 2 - minor_height],
-              color='b')  # 5
-    # line from (main_width, -(main_height - minor_height) / 2 - minor_height) to (main_width, -main_height)
-    ax2d.plot([main_width, main_width], [-(main_height - minor_height) / 2 - minor_height, -main_height],
-              color='b')  # 6
-    # line from (main_width, -main_height) to (0, -main_height)
-    ax2d.plot([main_width, 0], [-main_height, -main_height], color='b')  # 7
-
-    if secondObjectColliding == 'P0':
-        ax2d.plot([0, 0], [-main_height, 0], color='r')  # 0
-    elif secondObjectColliding == 'P1':
-        ax2d.plot([0, main_width], [0, 0], color='r')  # 1
-    elif secondObjectColliding == 'P2':
-        ax2d.plot([main_width, main_width], [0, -(main_height - minor_height) / 2], color='r')  # 2
-    elif secondObjectColliding == 'P3':
-        ax2d.plot([main_width, main_width + minor_width],
-                  [-(main_height - minor_height) / 2, -(main_height - minor_height) / 2], color='r')  # 3
-    elif secondObjectColliding == 'P4':
-        ax2d.plot([main_width + minor_width, main_width + minor_width],
-                  [-(main_height - minor_height) / 2, -(main_height - minor_height) / 2 - minor_height], color='r')  # 4
-    elif secondObjectColliding == 'P5':
-        ax2d.plot([main_width + minor_width, main_width],
-                  [-(main_height - minor_height) / 2 - minor_height, -(main_height - minor_height) / 2 - minor_height],
-                  color='r')  # 5
-    elif secondObjectColliding == 'P6':
-        ax2d.plot([main_width, main_width], [-(main_height - minor_height) / 2 - minor_height, -main_height],
-                  color='r')  # 6
-    elif secondObjectColliding == 'P7':
-        ax2d.plot([main_width, 0], [-main_height, -main_height], color='r')  # 7
-    else:
-        if secondObjectColliding != '-1':
-            ax2d.scatter(particles[int(secondObjectColliding), 0], particles[int(secondObjectColliding), 1], s=75,
-                         c='r')
-
-    if firstObjectColliding != -1:
-        ax2d.scatter(particles[int(firstObjectColliding), 0], particles[int(firstObjectColliding), 1], s=75, c='r')
-
-    plt.xticks([])
-    plt.yticks([])
-
-    ax2d.set_xticks([])
-    ax2d.set_yticks([])
-
-    return [img_plot]
 
 
-def animate(height, skip=100):
-    global img_plot, ax2d, minor_height, lines
-    fig, ax2d = plt.subplots(figsize=(10, 10))
-    ax2d.set_aspect('equal', adjustable='box')
-    fig.canvas.manager.set_window_title('Gas Diffusion')
+def update(frame):
+    global particle_positions, particles, lines
 
-    minor_height = height
-    file_suffix = str(height) + "_0"
+    first_line = frame * (num_particles * 2 + 2) + 1
+    for i in range(num_particles * 2):
+        particle_positions[i] = float(lines[i + first_line].split()[0])
 
-    with open('outputs/output_' + file_suffix + '.txt', 'r') as file:
+    for i, particle in enumerate(particles):
+        particle.center = (particle_positions[i], 0)
+
+    return particles
+
+
+def animate():
+    global particle_positions, particles, lines
+
+    file_suffix = str(num_particles) + '_' + str(delta_t)
+    with open('outputs/particle_train_' + file_suffix + '.txt', 'r') as file:
         lines = file.readlines()
 
-    max_step = int(len(lines)/(constants["n"] + 4))
-    cant_frames = int(max_step/skip)
+    first_line = 1
+    for i in range(num_particles * 2):
+        particle_positions[i] = float(lines[i + first_line].split()[0])
 
-    ani = FuncAnimation(fig, frames=cant_frames, func=update, fargs=(skip,), interval=100)
-    plt.tight_layout()
-    ani.save('gifs/animation_' + file_suffix + '.gif')
-    print('Gif ' + file_suffix + ' creado')
-    return ani
+    colors = plt.cm.viridis(np.linspace(0, 1, num_particles))
+
+    particles = [Circle((particle_positions[i], 0), radius=particle_radius, color=colors[i % num_particles], zorder=10)
+                 for i in range(num_particles * 2)]
+
+    for particle in particles:
+        ax.add_patch(particle)
+
+    max_frames = int(len(lines) / (num_particles * 2 + 2))
+
+    ani = FuncAnimation(fig, update, frames=max_frames, blit=True, interval=10)
+
+    # Hide axis
+    ax.axis('off')
+    ani.save('gifs/particle_train_' + file_suffix + '.gif')
+    print('Gif creado')
+
+    plt.show()
+
+
+animate()
