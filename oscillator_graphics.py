8913e7c5b0f0ef0f70accfba0e3b406fe189a523
TomyMarengo <marengotomas@gmail.com>
2023-10-01 14:13:58 -0300

    MSD -> MSE

--- oscillator_graphics.py
+++ oscillator_graphics.py
@@ -9,7 +9,8 @@ gamma = 100
 A = 1
 delta_ts = [0.01, 0.001, 0.0001, 0.00001, 0.000001]
 oscillators = ['beeman', 'gear', 'verlet']
-params = [('blue', '-'), ('cyan', '-.'), ('magenta', ':'), ('black', '--')]
+osc_graphic_params = [('blue', '-'), ('cyan', '-.'), ('magenta', ':'), ('black', '--')]
+osc_labels = ['Beeman', 'Gear predictor-corrector 5', 'Verlet Original', 'Analítica']
 
 def analytic_position(t):
     return A * np.exp(-(gamma / (2 * m)) * t) * np.cos(np.sqrt(k / m - ((gamma ** 2) / (4 * m ** 2))) * t)
@@ -28,31 +29,32 @@ def comparing():
         t = np.linspace(0, tf, int(tf / delta_t) + 1)
         analytic_values = analytic_position(t)
 
-        # Read the values from the files for all oscillator with the delta_t and compare MSD with the analytic values
+        # Read the values from the files for all oscillator with the delta_t and compare MSE with the analytic values
         # Also graph the position for all oscillators and the anylitic one for the delta_t
         for j, oscillator in enumerate(oscillators):
             positions = []
             for line in lines[j]:
                 positions.append(float(line))
 
-            # Calculate the MSD
-            MSD = np.mean((analytic_values - positions) ** 2)
-            print("MSD for " + str(oscillator) + " with delta_t = " + str(delta_t) + ": " + str(MSD))
+            # Calculate the MSE
+            MSE = np.mean((analytic_values - positions) ** 2)
+            print("MSE for " + str(oscillator) + " with delta_t = " + str(delta_t) + ": " + str(MSE))
 
             # Graph the position
-            plt.plot(t, positions, label=oscillator, color=params[j][0], linestyle=params[j][1])
+            plt.plot(t, positions, label=osc_labels[j], color=osc_graphic_params[j][0], linestyle=osc_graphic_params[j][1])
 
-        plt.plot(t, analytic_values, label="analítico", color=params[3][0], linestyle=params[3][1])
+        plt.plot(t, analytic_values, label=osc_labels[3], color=osc_graphic_params[3][0], linestyle=osc_graphic_params[3][1])
         plt.xlabel('Tiempo (s)')
         plt.ylabel('Posición (m)')
         plt.legend(loc='upper right')
+        plt.show()
         plt.savefig("images/position_" + str(delta_t) + ".png")
         print("Saved position_" + str(delta_t) + ".png")
         plt.close()
 
-    # Graph the MSD for all oscillators with the delta_ts
+    # Graph the MSE for all oscillators with the delta_ts
     for j, oscillator in enumerate(oscillators):
-        MSDs = []
+        MSEs = []
         for i, delta_t in enumerate(delta_ts):
             formatted_delta_t = "{:.6f}".format(delta_t).rstrip('0').rstrip('.')
             file_suffix = str(oscillator) + '_' + formatted_delta_t
@@ -63,19 +65,21 @@ def comparing():
             for line in lines:
                 positions.append(float(line))
 
-            # Calculate the MSD
+            # Calculate the MSE
             t = np.linspace(0, tf, int(tf / delta_t) + 1)
             analytic_values = analytic_position(t)
-            MSD = np.mean((analytic_values - positions) ** 2)
-            MSDs.append(MSD)
+            MSE = np.mean((analytic_values - positions) ** 2)
+            MSEs.append(MSE)
 
-        plt.loglog(delta_ts, MSDs, label=oscillator)
+        plt.loglog(delta_ts, MSEs, label=osc_labels[j], color=osc_graphic_params[j][0], linestyle=osc_graphic_params[j][1])
 
-    plt.xlabel('$\Delta t (s)')
-    plt.ylabel('MSD')
-    plt.legend(loc='upper right')
-    plt.savefig("images/MSD.png")
-    print("Saved MSD.png")
+    plt.xlabel('$\Delta$t (s)')
+    plt.ylabel('MSE')
+    # legend without overlapping (font size 10) upper left
+    plt.legend(loc='upper left', fontsize=10)
+
+    plt.savefig("images/MSE.png")
+    print("Saved MSE.png")
     plt.close()
 
 
