2df830db3dea068e427f519cd11e52b089b4b687
TomyMarengo <marengotomas@gmail.com>
2023-10-01 23:45:41 -0300

    Update animation.py

--- animation.py
+++ animation.py
@@ -20,7 +20,6 @@ def update(frame):
         particle.center = (particle_positions[i], 0)
 
     # Change title with time in seconds in each frame
-    plt.title('Tiempo: ' + str(frame * 1) + ' s')  # TODO: Delete this
     return particles
 
 
